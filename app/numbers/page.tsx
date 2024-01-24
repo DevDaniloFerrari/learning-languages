03cf5c9392fd2774d508a69348fced902aed925a
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-01-24 20:02:32 -0300

    fix formatting

--- app/numbers/page.tsx
+++ app/numbers/page.tsx
@@ -1,29 +1,32 @@
 'use client'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NumberDisplay from "@/components/NumberDisplay"
 import generateRandomNumbers from "@/functions/randomNumbers"
 
 export default function numbers() {
-    const [length, setLength] = useState(1)
+    const [length, setLength] = useState(5)
     const [numbers, setNumbers] = useState<number[]>([])
+
+    useEffect(() => {
+        setNumbers(generateRandomNumbers(length))
+    }, [])
 
     return (
         <div style={{
             display: 'flex',
-            justifyContent: 'center',
-            alignContent: 'center',
+            alignItems: 'center',
             flexDirection: 'column'
         }}>
             <h1>Generate Numbers</h1>
             <div style={{
                 display: 'flex',
-                justifyContent: 'center',
-                alignContent: 'center',
+                alignItems: 'center',
+                flexWrap: 'wrap'
             }}>
                 {numbers.map(number => <NumberDisplay value={number} />)}
             </div>
             <div>
-                <input type="number" max={15} min={5} onChange={e => setLength(+e.target.value)} value={length}/>
+                <input type="number" min={1} max={100} onChange={e => setLength(+e.target.value)} value={length}/>
                 <button onClick={() => setNumbers(generateRandomNumbers(length))}>Gerenate</button>
             </div>
         </div>
