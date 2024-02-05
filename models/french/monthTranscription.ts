fea60f68bc3a0810d0de01fbbd7b1c8b7b80d05c
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-05 19:40:09 -0300

    month transctiption object

---
+++ models/french/monthTranscription.ts
@@ -0,0 +1,18 @@
+interface MonthTranscriptionsProps {
+    [key: number]: string;
+  }
+  
+  export const monthTranscriptions: MonthTranscriptionsProps = {
+    1: 'janvier',
+    2: 'février',
+    3: 'mars',
+    4: 'avril',
+    5: 'mai',
+    6: 'juin',
+    7: 'juillet',
+    8: 'août',
+    9: 'septembre',
+    10: 'octobre',
+    11: 'november',
+    12: 'décembre',
+  };
